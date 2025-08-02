6ad0da609757ccc4486f0b5e0f195ea3331937e3
erjemin <erjemin@gmail.com>
2025-08-02 11:01:32 +0300

    add: тест кодека html-мнемоник.

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -19,58 +19,73 @@ STRINGS_FOR_DECODE = [
     ("&lsquo; &rsquo; &sbquo;", "‘ ’ ‚"),  # Одиночные кавычки
     ("&lsaquo; &rsaquo;", "‹ ›"),  # Французские угловые кавычки
     ("&dollar; &cent; &pound; &curren; &yen; &euro; &#8381;", "$ ¢ £ ¤ ¥ € ₽"),  # Валютные символы
-    ("&plus; &minus; &times; &divide; &equals; &ne;", "+ − × ÷ = ≠"),   # Математические символы 01
-    ("&plusmn; &not; &deg; &sup1; &sup2 &sup3;", "± ¬ ° ¹ ² ³"),        # Математические символы 02
-    ("&fnof; &percnt; &permil; &pertenk;", "ƒ % ‰ ‱"),                  # Математические символы 03
-    ("&forall; &comp; &part; &exist; &nexist;", "∀ ∁ ∂ ∃ ∄"),           # Математические символы 04
-    ("&empty; &nabla; &isin; &notin; &ni; &notni;", "∅ ∇ ∈ ∉ ∋ ∌"),     # Математические символы 05
-    ("&prod; &coprod; &sum; &mnplus; &minusd;", "∏ ∐ ∑ ∓ ∸"),           # Математические символы 06
-    ("&plusdo; &setminus; &lowast; &compfn; &radic;", "∔ ∖ ∗ ∘ √"),     # Математические символы 07
-    ("&prop; &infin; &ang; &angrt; &angmsd; &mid;", "∝ ∞ ∠ ∟ ∡ ∣"),     # Математические символы 08
-    ("&angsph; &nmid; '&parallel; &npar; &and; &or;", "∢ ∤ '∥ ∦ ∧ ∨"),  # Математические символы 09
-    ("&cap; &cup; &int; &Int; &iiint; &conint;", "∩ ∪ ∫ ∬ ∭ ∮"),        # Математические символы 10
-    ("&Conint; &Cconint; &cwint; &cwconint;", "∯ ∰ ∱ ∲"),              # Математические символы 11
-    ("&awconint; &there4; &because; &ratio; &Colon;", "∳ ∴ ∵ ∶ ∷"),     # Математические символы 12
-    ("&mDDot; &homtht; &sim; &bsim; &ac; &acd;", "∺ ∻ ∼ ∽ ∾ ∿"),        # Математические символы 13
-    ("&wreath; &nsim; &esim; &sime; &nsime; &cong;", "≀ ≁ ≂ ≃ ≄ ≅"),    # Математические символы 14
-    ("&asymp; &simne; &ncong; &nap; &approxeq; &apid;", "≈ ≆ ≇ ≉ ≊ ≋"), # Математические символы 15
-    ("&bcong; &asympeq; &bump; &bumpe; &esdot; &eDot;", "≌ ≍ ≎ ≏ ≐ ≑"), # Математические символы 16
-    ("&efDot; &erDot; &colone; &ecolon; &ecir; &cire;", "≒ ≓ ≔ ≕ ≖ ≗"), # Математические символы 17
-    ("&wedgeq; &veeeq; &trie; &equest; &equiv;", "≙ ≚ ≜ ≟ ≡"),          # Математические символы 18
-    ("&nequiv; &le; &ge; &lE; &gE; &lnE; &gnE;", "≢ ≤ ≥ ≦ ≧ ≨ ≩"),      # Математические символы 19
-    ("&Lt; &Gt; &between; &NotCupCap; &nlt; &ngt;", "≪ ≫ ≬ ≭ ≮ ≯"),     # Математические символы 20
-    ("&nle; &nge; &lsim; &gsim; &nlsim; &ngsim;", "≰ ≱ ≲ ≳ ≴ ≵"),       # Математические символы 21
-    ("&lg; &gl; &ntlg; &ntgl; &pr; &sc;", "≶ ≷ ≸ ≹ ≺ ≻"),               # Математические символы 22
-    ("&prcue; &sccue; &prsim; &scsim; &npr; &nsc;", "≼ ≽ ≾ ≿ ⊀ ⊁"),     # Математические символы 23
-    ("&sub; &sup; &nsub; &nsup; &sube; &supe;", "⊂ ⊃ ⊄ ⊅ ⊆ ⊇"),         # Математические символы 24
-    ("&nsube; &nsupe; &subne; &supne; &cupdot;", "⊈ ⊉ ⊊ ⊋ ⊍"),          # Математические символы 25
-    ("&uplus; &sqsub; &sqsup; &sqsube; &sqsupe;", "⊎ ⊏ ⊐ ⊑ ⊒"),         # Математические символы 26
-    ("&sqcap; &sqcup; &oplus; &ominus; &otimes;", "⊓ ⊔ ⊕ ⊖ ⊗"),         # Математические символы 27
-    ("&osol; &odot; &ocir; &oast; &odash; &plusb;", "⊘ ⊙ ⊚ ⊛ ⊝ ⊞"),     # Математические символы 28
-    ("&minusb; &timesb; &sdotb; &vdash; &dashv; &top;", "⊟ ⊠ ⊡ ⊢ ⊣ ⊤"), # Математические символы 29
-    ("&bot; &models; &vDash; &Vdash; &Vvdash;", "⊥ ⊧ ⊨ ⊩ ⊪"),           # Математические символы 30
-    ("&VDash; &nvdash; &nvDash; &nVdash; &nVDash;", "⊫ ⊬ ⊭ ⊮ ⊯"),     # Математические символы 31
-    ("&prurel; &vltri; &vrtri; &ltrie; &rtrie;", "⊰ ⊲ ⊳ ⊴ ⊵"),         # Математические символы 32
-    ("&origof; &imof; &mumap; &hercon; &intcal;", "⊶ ⊷ ⊸ ⊹ ⊺"),        # Математические символы 33
-    ("&veebar; &barvee; &angrtvb; &lrtri; &xwedge;", "⊻ ⊽ ⊾ ⊿ ⋀"),     # Математические символы 34
-    ("&xvee; &xcap; &xcup; &diamond; &sdot; &Star;", "⋁ ⋂ ⋃ ⋄ ⋅ ⋆"),    # Математические символы 35
-    ("&divonx; &bowtie; &ltimes; &rtimes; &lthree;", "⋇ ⋈ ⋉ ⋊ ⋋"),      # Математические символы 36
-    ("&rthree; &bsime; &cuvee; &cuwed; &Sub; &Sup;", "⋌ ⋍ ⋎ ⋏ ⋐ ⋑"),    # Математические символы 37
-    ("&Cap; &Cup; &fork; &epar; &ltdot; &gtdot;", "⋒ ⋓ ⋔ ⋕ ⋖ ⋗"),       # Математические символы 38
-    ("&Ll; &Gg; &leg; &gel; &cuepr; &cuesc;", "⋘ ⋙ ⋚ ⋛ ⋞ ⋟"),          # Математические символы 39
-    ("&nprcue; &nsccue; &nsqsube; &nsqsupe;", "⋠ ⋡ ⋢ ⋣"),              # Математические символы 40
-    ("&lnapprox; &gnapprox; &lnsim; &gnsim; &prnsim;", "⪉ ⪊ ⋦ ⋧ ⋨"),    # Математические символы 41
-    ("&scnsim; &nltri; &nrtri; &nltrie; &nrtrie;", "⋩ ⋪ ⋫ ⋬ ⋭"),      # Математические символы 42
-    ("&vellip; &ctdot; &utdot; &dtdot; &disin;", "⋮ ⋯ ⋰ ⋱ ⋲"),         # Математические символы 43
-    ("&isinsv; &isins; &isindot; &notinvc; &notinvb;", "⋳ ⋴ ⋵ ⋶ ⋷"),   # Математические символы 44
-    ("&isinE; &nisd; &xnis; &nis; &notnivc;", "⋹ ⋺ ⋻ ⋼ ⋽"),            # Математические символы 45
+    ("&plus; &minus; &times; &divide; &equals; &ne;", "+ − × ÷ = ≠"),   # Математические символы
+    ("&plusmn; &not; &deg; &sup1; &sup2 &sup3;", "± ¬ ° ¹ ² ³"),
+    ("&fnof; &percnt; &permil; &pertenk;", "ƒ % ‰ ‱"),
+    ("&forall; &comp; &part; &exist; &nexist;", "∀ ∁ ∂ ∃ ∄"),
+    ("&empty; &nabla; &isin; &notin; &ni; &notni;", "∅ ∇ ∈ ∉ ∋ ∌"),
+    ("&prod; &coprod; &sum; &mnplus; &minusd;", "∏ ∐ ∑ ∓ ∸"),
+    ("&plusdo; &setminus; &lowast; &compfn; &radic;", "∔ ∖ ∗ ∘ √"),
+    ("&prop; &infin; &ang; &angrt; &angmsd; &mid;", "∝ ∞ ∠ ∟ ∡ ∣"),
+    ("&angsph; &nmid; '&parallel; &npar; &and; &or;", "∢ ∤ '∥ ∦ ∧ ∨"),
+    ("&cap; &cup; &int; &Int; &iiint; &conint;", "∩ ∪ ∫ ∬ ∭ ∮"),
+    ("&Conint; &Cconint; &cwint; &cwconint;", "∯ ∰ ∱ ∲"),
+    ("&awconint; &there4; &because; &ratio; &Colon;", "∳ ∴ ∵ ∶ ∷"),
+    ("&mDDot; &homtht; &sim; &bsim; &ac; &acd;", "∺ ∻ ∼ ∽ ∾ ∿"),
+    ("&wreath; &nsim; &esim; &sime; &nsime; &cong;", "≀ ≁ ≂ ≃ ≄ ≅"),
+    ("&asymp; &simne; &ncong; &nap; &approxeq; &apid;", "≈ ≆ ≇ ≉ ≊ ≋"),
+    ("&bcong; &asympeq; &bump; &bumpe; &esdot; &eDot;", "≌ ≍ ≎ ≏ ≐ ≑"),
+    ("&efDot; &erDot; &colone; &ecolon; &ecir; &cire;", "≒ ≓ ≔ ≕ ≖ ≗"),
+    ("&wedgeq; &veeeq; &trie; &equest; &equiv;", "≙ ≚ ≜ ≟ ≡"),
+    ("&nequiv; &le; &ge; &lE; &gE; &lnE; &gnE;", "≢ ≤ ≥ ≦ ≧ ≨ ≩"),
+    ("&Lt; &Gt; &between; &NotCupCap; &nlt; &ngt;", "≪ ≫ ≬ ≭ ≮ ≯"),
+    ("&nle; &nge; &lsim; &gsim; &nlsim; &ngsim;", "≰ ≱ ≲ ≳ ≴ ≵"),
+    ("&lg; &gl; &ntlg; &ntgl; &pr; &sc;", "≶ ≷ ≸ ≹ ≺ ≻"),
+    ("&prcue; &sccue; &prsim; &scsim; &npr; &nsc;", "≼ ≽ ≾ ≿ ⊀ ⊁"),
+    ("&sub; &sup; &nsub; &nsup; &sube; &supe;", "⊂ ⊃ ⊄ ⊅ ⊆ ⊇"),
+    ("&nsube; &nsupe; &subne; &supne; &cupdot;", "⊈ ⊉ ⊊ ⊋ ⊍"),
+    ("&uplus; &sqsub; &sqsup; &sqsube; &sqsupe;", "⊎ ⊏ ⊐ ⊑ ⊒"),
+    ("&sqcap; &sqcup; &oplus; &ominus; &otimes;", "⊓ ⊔ ⊕ ⊖ ⊗"),
+    ("&osol; &odot; &ocir; &oast; &odash; &plusb;", "⊘ ⊙ ⊚ ⊛ ⊝ ⊞"),
+    ("&minusb; &timesb; &sdotb; &vdash; &dashv; &top;", "⊟ ⊠ ⊡ ⊢ ⊣ ⊤"),
+    ("&bot; &models; &vDash; &Vdash; &Vvdash;", "⊥ ⊧ ⊨ ⊩ ⊪"),
+    ("&VDash; &nvdash; &nvDash; &nVdash; &nVDash;", "⊫ ⊬ ⊭ ⊮ ⊯"),
+    ("&prurel; &vltri; &vrtri; &ltrie; &rtrie;", "⊰ ⊲ ⊳ ⊴ ⊵"),
+    ("&origof; &imof; &mumap; &hercon; &intcal;", "⊶ ⊷ ⊸ ⊹ ⊺"),
+    ("&veebar; &barvee; &angrtvb; &lrtri; &xwedge;", "⊻ ⊽ ⊾ ⊿ ⋀"),
+    ("&xvee; &xcap; &xcup; &diamond; &sdot; &Star;", "⋁ ⋂ ⋃ ⋄ ⋅ ⋆"),
+    ("&divonx; &bowtie; &ltimes; &rtimes; &lthree;", "⋇ ⋈ ⋉ ⋊ ⋋"),
+    ("&rthree; &bsime; &cuvee; &cuwed; &Sub; &Sup;", "⋌ ⋍ ⋎ ⋏ ⋐ ⋑"),
+    ("&Cap; &Cup; &fork; &epar; &ltdot; &gtdot;", "⋒ ⋓ ⋔ ⋕ ⋖ ⋗"),
+    ("&Ll; &Gg; &leg; &gel; &cuepr; &cuesc;", "⋘ ⋙ ⋚ ⋛ ⋞ ⋟"),
+    ("&nprcue; &nsccue; &nsqsube; &nsqsupe;", "⋠ ⋡ ⋢ ⋣"),
+    ("&lnapprox; &gnapprox; &lnsim; &gnsim; &prnsim;", "⪉ ⪊ ⋦ ⋧ ⋨"),
+    ("&scnsim; &nltri; &nrtri; &nltrie; &nrtrie;", "⋩ ⋪ ⋫ ⋬ ⋭"),
+    ("&vellip; &ctdot; &utdot; &dtdot; &disin;", "⋮ ⋯ ⋰ ⋱ ⋲"),
+    ("&isinsv; &isins; &isindot; &notinvc; &notinvb;", "⋳ ⋴ ⋵ ⋶ ⋷"),
+    ("&isinE; &nisd; &xnis; &nis; &notnivc;", "⋹ ⋺ ⋻ ⋼ ⋽"),
     ("&notnivb; &barwed; &Barwed; &lceil; &lceil;", "⋾ ⌅ ⌆ ⌈ ⌈"),
     ("&rceil; &lfloor; &rfloor; &lang; &rang;", "⌉ ⌊ ⌋ ⟨ ⟩"),
+    ("&copy; &reg; &trade; &copysr; &commat;", "© ® ™ ℗ @"),                    # Другие символы
+    ("&Copf; &incare; &gscr; &hamilt; &Hfr; &Hopf;", "ℂ ℅ ℊ ℋ ℌ ℍ"),
+    ("&planckh; &planck; &Iscr; &image; &Lscr; &ell;", "ℎ ℏ ℐ ℑ ℒ ℓ"),
+    ("&Nopf; &numero; &weierp; &Popf; &Qopf; &Rscr;", "ℕ № ℘ ℙ ℚ ℛ"),
+    ("&Ropf; &rx; &Zopf; &mho; &Zfr; &iiota;", "ℝ ℞ ℤ ℧ ℨ ℩"),
+    ("&bernou; &Cfr; &escr; &Escr; &Fscr; &Mscr;", "ℬ ℭ ℯ ℰ ℱ ℳ"),
+    ("&oscr; &alefsym; &beth; &gimel; &daleth;", "ℴ ℵ ℶ ℷ ℸ"),
+    ("&DD; &dd; &ee; &ii; &ffilig; &fflig;", "ⅅ ⅆ ⅇ ⅈ ﬃ ﬀ"),
+    ("&filig; &fllig; &starf; &star; &phone;", "ﬁ ﬂ ★ ☆ ☎"),
+    ("&female; &male; &spades; &clubs; &hearts; &diams;", "♀ ♂ ♠ ♣ ♥ ♦"),
+    ("&loz; &sung; &flat; &natural; &sharp; &check;", "◊ ♪ ♭ ♮ ♯ ✓"),
+    ("&cross; &malt; &sext; &VerticalSeparator;", "✗ ✠ ✶ ❘"),
+    ("&lbbrk; &rbbrk;", "❲ ❳"),
+    ("", ""),
     ("", ""),
     ("", ""),
 
 
-    ("&copy; &reg; &trade;", "\u00A9 \u00AE \u2122"),  # Символы авторского права, зарегистрированной торговой марки и товарного знака
+
+
 ]
 
 @pytest.mark.parametrize("input_string, expected_output", STRINGS_FOR_DECODE)
