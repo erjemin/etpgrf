bb1fcc71e04ccf7e84773053b91e3f7751236e73
erjemin <erjemin@gmail.com>
2026-02-27 01:04:13 +0300

    mod: v0.1.5

--- etpgrf/__init__.py
+++ etpgrf/__init__.py
@@ -8,19 +8,8 @@ etpgrf - библиотека для экранной типографики т�
 - Висячая пунктуация
 - Очистка и обработка HTML
 """
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "Sergei Erjemin"
 __email__ = "erjemin@gmail.com"
 __license__ = "MIT"
-__copyright__ = "Copyright 2025 Sergei Erjemin"
-
-import etpgrf.defaults
-import etpgrf.logger
-
-from etpgrf.hyphenation import Hyphenator
-from etpgrf.layout import LayoutProcessor
-from etpgrf.quotes import QuotesProcessor
-from etpgrf.sanitizer import SanitizerProcessor
-from etpgrf.symbols import SymbolsProcessor
-from etpgrf.typograph import Typographer
-from etpgrf.unbreakables import Unbreakables
+__copyright__ = "(с) 2025-2026, Sergei Erjemin"
